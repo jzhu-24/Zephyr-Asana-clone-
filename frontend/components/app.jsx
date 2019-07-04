211d8ac79642c6ee1c80cd4a46fee79fc95c4b4a
Julian <thezhukeeper@gmail.com>
2019-07-04 15:05:13 -0700

    Initial commit to zephyrsana

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,19 +3,22 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import WorkspaceIndex from './workspaces/workspace_index_container';
-import Home from './home/home';
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
+
+const dummyComp = () => (
+    <h2>Hi</h2>
+);
+
+// ??? study this shit
 
 export default () => (
     <div>
         <Switch>
-            {/* todo - how to route everything to /login */}
-            <AuthRoute exact path="/" component={SignupContainer} />
-            {/* <AuthRoute path="/signup" component={SignupContainer} /> */}
+            <AuthRoute exact path="/signup" component={SignupContainer} />
             <AuthRoute exact path="/login" component={LoginContainer} />
-            {/* <AuthRoute path="/signup" component={SignupContainer} /> */}
-            {/* <Route exact path="/" component={WorkspaceIndex} /> */}
-            <ProtectedRoute path="/workspaces" component={WorkspaceIndex} />
+            <ProtectedRoute path="/" component={WorkspaceIndex} />
+            <AuthRoute path="/" component={SignupContainer} />
+            <Redirect path="/signup" />
         </Switch>
     </div>
 );
