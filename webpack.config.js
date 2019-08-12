ce5fddc7a8d53a249b73226985960d8b1afd1d43
Julian Zhu <thezhukeeper@gmail.com>
2019-08-12 16:29:55 -0700

    Add webpack require

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './frontend/zephyr.jsx',
-    output: {
-        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: [/\.jsx?$/],
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/env', '@babel/react', {
-                        'plugins': ['@babel/plugin-proposal-class-properties']
-                    }]
-                }
-            },
-            {
-                test: /\.css$/i,
-				loader: 'style-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: 'css-loader',
-            },
-        ]
-    },
-    devtool: 'inline-source-map',
-    resolve: {
-        extensions: ['.js', '.jsx', '*'],
-    },
-    plugins:[
-        new webpack.DefinePlugin({
-          'process.env':{
-            'NODE_ENV': JSON.stringify('production')
-          }
-      }),
-    ]
+  entry: './frontend/zephyr.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react', {
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }],
+        },
+      },
+      {
+        test: /\.css$/i,
+        loader: 'style-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: 'css-loader',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
 };
